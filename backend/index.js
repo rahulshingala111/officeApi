62dd36781ff4d12a88bf2823b109cd1d002cc20f
unknown <rahulshingala111@gmail.com>
2022-09-29 09:45:56 +0530

    first tocken test

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ mongoose.connect("mongodb://localhost:27017/firstreactdb", {
   //useCreateIndex: true,
   useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
